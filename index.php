9bcccd5a4d219649392a682e507985ef0c5eac64
Hendrik Wendt <h.wendt@gmx.com>
2022-10-14 16:07:22 -0400

    misc changes

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ get_header();
 
 if (have_posts()) :
     while (have_posts()) : the_post();
-        the_title();
+        the_title('<h1>', '</h1>');
+
+        the_content();
     endwhile;
 endif;
 
